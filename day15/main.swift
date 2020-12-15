624a7f4326b3ad5108f556d3b56e0e82005b7357
Sven Weidauer <sven@5sw.de>
2020-12-15 07:19:45 +0100

    day 15

--- day15/main.swift
+++ day15/main.swift
@@ -1,4 +1,36 @@
 import Foundation
 
-let input = loadData(day: 15)
+let input = [16,1,0,18,12,14,19]
+
+var memory: [Int: (Int, Int?)] = [:]
+var last = -1
+
+for (round, num) in input.enumerated() {
+    memory[num] = (round, nil)
+    last = num
+}
+
+func lastRound(for num: Int) -> Int? {
+    guard let (first, second) = memory[num] else { return nil }
+    if let second = second { return second }
+    return first
+}
+
+for round in input.count..<30000000 {
+    let (first, second) = memory[last]!
+
+    if let second = second {
+        last = second - first
+    } else {
+        last = 0
+    }
+
+    if let prev = lastRound(for: last) {
+        memory[last] = (prev, round)
+    } else {
+        memory[last] = (round, nil)
+    }
+}
+
+print(last)
 
