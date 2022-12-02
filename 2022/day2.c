3d057cfd046e0edfad4834ddaae4b55b9fb4309f
Sven Weidauer <sven@5sw.de>
2022-12-02 11:47:37 +0100

    2022 Day 2 Part 2

--- 2022/day2.c
+++ 2022/day2.c
@@ -7,16 +7,44 @@ int outcomes[3][3] = {
 /* Scissor */   {   0,      6,      3 }
 };
 
+int findMove(char opponent, int outcome) {
+    for (int i = 0; i < 3; i++) {
+        if (outcomes[i][opponent - 'A'] == outcome) {
+            return i;
+        }
+    }
+    return -1;
+}
+
 int main() {
     FILE *input = fopen("day2.input", "r");
-    int total = 0;
+    int totalPart1 = 0;
+    int totalPart2 = 0;
     while (!feof(input)) {
         char opponent, my;
         fscanf(input, "%c %c\n", &opponent, &my);
         
-        total += my - 'X' + 1 + outcomes[my - 'X'][opponent - 'A'];
+        totalPart1 += my - 'X' + 1 + outcomes[my - 'X'][opponent - 'A'];
+
+        switch (my) {
+            case 'X': // Lose
+                my = findMove(opponent, 0);
+                break;
+                
+            case 'Y': // Draw
+                my = opponent - 'A';
+                break;
+                
+            case 'Z': // Win
+                my = findMove(opponent, 6);
+                break;
+        }
+
+        totalPart2 += my + 1 + outcomes[my][opponent - 'A'];
     }
+    
     fclose(input);
     
-    printf("Total score %d\n", total);
+    printf("Part1: Total score %d\n", totalPart1);
+    printf("Part2: Total score %d\n", totalPart2);
 }
