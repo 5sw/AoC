b37bdc8b3ef487d11223899e6619c9311e17af8b
Sven Weidauer <sven@5sw.de>
2024-12-03 19:01:43 +0100

    Improve error message when input cannot be read.

--- 2024/src/main/kotlin/Utils.kt
+++ 2024/src/main/kotlin/Utils.kt
@@ -1,8 +1,8 @@
 fun readInput(name: String) = bufferedReader(name)
     ?.lineSequence()
-    ?: error("Cannot read input")
+    ?: error("Cannot read input $name")
 
-fun readInputString(name: String): String = bufferedReader(name)?.readText() ?: error("Cannot read input")
+fun readInputString(name: String): String = bufferedReader(name)?.readText() ?: error("Cannot read input $name")
 
 private fun bufferedReader(name: String) = object {}.javaClass.getResourceAsStream(name)
     ?.bufferedReader()
